267bbc10c804218b68eb93d5f27b9c6a5ea1a245
obaloluwajohn <john.akadi01@gmail.com>
2025-10-25 09:06:07 +0000

    save

--- main.py
+++ main.py
@@ -11,28 +11,21 @@ window = display.set_mode((600, 600))
 # practising with arguments and properties
 
 # variable definitions and assignment
+tail = pygame.Rect(50,150,150,50)
+backleg = pygame.Rect(210,150, 50, 200)
+stomach = pygame.Rect(270,150,50,120)
+frontleg = pygame.Rect(330,150,50,200)
+neck = pygame.Rect(340,110,50,80)
+head = pygame.Rect(400,80,90,65)
+nose = pygame.Rect(450,80,90,55)
 
-house = pygame.Rect(90, 180, 300, 400)
-square = pygame.Rect(90, 180, 300, 400)
-door = pygame.Rect(190, 215, 100, 140)
-window1 = pygame.Rect(110, 215, 60, 60)
-window2 = pygame.Rect(310, 215, 60, 60)
-window1_1 = pygame.Rect(110, 215, 30, 60)
-window1_2 = pygame.Rect(310, 215, 30, 60)
-doorKnob = pygame.Rect(265, 284, 15, 5)
-roof = pygame.Rect(80, 140, 320, 50)
-
-draw.rect(window, "white", house)
-draw.rect(window, "orangered", door)
-draw.rect(window, "blue", window1)
-draw.rect(window, "blue", window2)
-draw.rect(window, "dark grey", doorKnob)
-draw.rect(window, "grey", window1_1)
-draw.rect(window, "grey", window1_2)
-draw.rect(window, "brown", roof)
-
-
-
+draw.rect(window,"blue",tail)
+draw.rect(window, "blue", backleg)
+draw.rect(window,"red",stomach)
+draw.rect(window,"blue",frontleg)
+draw.rect(window,"red",neck)
+draw.rect(window,"red",head)
+draw.rect(window,red",nose)
 
 #  A sample of while loops to Update the display
 while True:
